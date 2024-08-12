ad0ecb4700811026e9c013e6cb41cc4d25cf527b
terfess0 <santiagofontalvo13@gmail.com>
2024-08-12 12:17:27 -0500

    add color on radio when focus by touch

--- app/src/main/java/com/terfess/miradioyopal/recycler_view/AdapterHolderRadios.kt
+++ app/src/main/java/com/terfess/miradioyopal/recycler_view/AdapterHolderRadios.kt
@@ -1,7 +1,6 @@
 package com.terfess.miradioyopal.recycler_view
 
 import android.content.Context
-import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.view.LayoutInflater
 import android.view.View
@@ -13,8 +12,8 @@ import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.request.RequestOptions
-import com.google.android.material.circularreveal.CircularRevealHelper.Strategy
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 import com.terfess.miradioyopal.R
 import com.terfess.miradioyopal.actividades.HomeScreen
 import com.terfess.miradioyopal.databinding.ActivityHomeScreenBinding
@@ -27,6 +26,10 @@ class AdapterHolderRadios(
 ) :
     RecyclerView.Adapter<AdapterHolderRadios.ViewHolder>() {
 
+    var radioFocus: ConstraintLayout? = null
+    var lastClickTime: Long = 0
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view =
             LayoutInflater.from(context)
@@ -38,7 +41,7 @@ class AdapterHolderRadios(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = itemList[position]
-        holder.bind(item, context, position)
+        holder.bind(item, context)
     }
 
 
@@ -47,17 +50,22 @@ class AdapterHolderRadios(
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val debounceInterval =
+            600L // Intervalo de tiempo mínimo entre clics en milisegundos
+
         private val binding = ActivityHomeScreenBinding.bind(vista)
         private val nameRadio = itemView.findViewById<TextView>(R.id.nameRadio)
         private val imgRadio = itemView.findViewById<ImageButton>(R.id.station)
-        var cardRadio = itemView.findViewById<ConstraintLayout>(R.id.contain_radio)
+        val cardRadio = itemView.findViewById<ConstraintLayout>(R.id.contain_radio)
+
 
         // go facebook option
         private val facebook = binding.facebookOpcion
         private val functions = homeScreenInstance
         //
 
-        fun bind(item: DatoStationLocal, contexto: Context, oldPosition: Int) {
+
+        fun bind(item: DatoStationLocal, contexto: Context) {
             //set radio name
             nameRadio.text = item.name
 
@@ -70,36 +78,76 @@ class AdapterHolderRadios(
 
             // On click Radio
             imgRadio.setOnClickListener {
-                //onSelectRadio(cardRadio)
 
-                binding.cargando.visibility = View.VISIBLE
-                binding.textoHorizontal.text = context.getString(R.string.buscando)
-                binding.play.visibility = View.GONE
+                val currentTime = System.currentTimeMillis()
+                if (currentTime - lastClickTime > debounceInterval) {
+                    lastClickTime = currentTime
 
-                //start sound player
-                functions.starPlayer(item)
+                    removeFocusRadio()
+                    radioFocus = cardRadio
+                    onSelectRadio()
+                    println("Click valido")
 
-                //On click go facebook option
-                facebook.setOnClickListener {
-                    val enlaceFacebook = item.linkFacebook
-                    functions.abrirFacebook(enlaceFacebook)
+                    binding.cargando.visibility = View.VISIBLE
+                    binding.textoHorizontal.text = context.getString(R.string.buscando)
+                    binding.play.visibility = View.GONE
+
+                    //start sound player
+                    functions.starPlayer(item)
+
+                    //On click go facebook option
+                    facebook.setOnClickListener {
+                        val enlaceFacebook = item.linkFacebook
+                        functions.abrirFacebook(enlaceFacebook)
+                    }
+                } else {
+
+                    val snk = Snackbar.make(
+                        binding.root,
+                        "Calmate, toques muy rápidos!",
+                        Snackbar.LENGTH_SHORT
+                    )
+                    snk.animationMode = BaseTransientBottomBar.ANIMATION_MODE_FADE
+                    snk.setBackgroundTint(
+                        ContextCompat.getColor(
+                            binding.root.context,
+                            R.color.color_primario
+                        )
+                    )
+                    snk.show()
+                    println("Click no valido")
                 }
             }
         }
 
-        private fun onSelectRadio(cardRadio: ConstraintLayout) {
-            val borderWidth = 8 // Ancho del borde en píxeles
+        private fun onSelectRadio() {
+            radioFocus?.let { cardRadio ->
+                val borderWidth = 8 // Ancho del borde en píxeles
+                val selectedBorderColor =
+                    ContextCompat.getColor(binding.root.context, R.color.colorBorde)
 
+                // Crear y asignar el drawable solo si es necesario
+                val selectedDrawable = GradientDrawable().apply {
+                    setStroke(borderWidth, selectedBorderColor)
+                    cornerRadius = 16f
+                }
 
-            // Cambiar el color del borde del CardView cuando se selecciona
-
-            val selectedBorderColor = ContextCompat.getColor(binding.root.context, R.color.colorBorde) // Color del borde cuando se selecciona
-            val selectedDrawable = GradientDrawable().apply {
-                setStroke(borderWidth, selectedBorderColor)
-                cornerRadius = 16f
+                cardRadio.background = selectedDrawable
+                println("is selected: ")
             }
-            cardRadio.background = selectedDrawable
-            println("is selected: ")
         }
+
+        private fun removeFocusRadio() {
+            radioFocus?.let { cardRadio ->
+                // Crear y asignar el drawable solo si es necesario
+                val selectedDrawable = GradientDrawable().apply {
+                    setStroke(0, null)
+                }
+
+                cardRadio.background = selectedDrawable
+                println("is remove focus radio: ")
+            }
+        }
+
     }
 }
